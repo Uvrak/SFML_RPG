f6bfc84fcfa7ceed49f2f50113f972a85fd682a3
Daniel Horn <danielhorn101@gmail.com>
2020-07-07 20:26:32 +0200

    Keybinds and input from file

--- GameState.cpp
+++ GameState.cpp
@@ -2,21 +2,18 @@
 
 void GameState::initKeybinds()
 {
-	std::ifstream ifs("Config/supported_keys.ini");
+	std::ifstream ifs("Config/gamestate_keybinds.ini");
 	if (ifs.is_open()) {
 		std::string key = "";
-		int key_value = 0;
+		std::string key2 = "";
 
-		while(ifs >> key >> key_value)
+		while (ifs >> key >> key2)
 		{
+			this->keybinds[key] = this->supportedKeys->at(key2);
 		}
-		ifs.close();
 	}
-	this->keybinds["CLOSE"] = this->supportedKeys->at("Escape");
-	this->keybinds["MOVE_LEFT"] = this->supportedKeys->at("A");
-	this->keybinds["MOVE_RIGHT"] = this->supportedKeys->at("D");
-	this->keybinds["MOVE_UP"] = this->supportedKeys->at("W");
-	this->keybinds["MOVE_DOWN"] = this->supportedKeys->at("S");
+
+	ifs.close();
 }
 
 GameState::GameState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys) : State(window, supportedKeys)
