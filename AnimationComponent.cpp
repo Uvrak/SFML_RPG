73ab98a675c6ba862db62d517a3e1255e4f90a0d
Daniel Horn <danielhorn101@gmail.com>
2020-07-09 11:57:12 +0200

    Animation component part 2

--- AnimationComponent.cpp
+++ AnimationComponent.cpp
@@ -1,9 +1,30 @@
 #include "AnimationComponent.h"
 
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet)
+	: sprite(sprite), textureSheet(textureSheet)
 {
 }
 
 AnimationComponent::~AnimationComponent()
+{
+}
+
+void AnimationComponent::addAnimation(const std::string key)
+{
+}
+
+void AnimationComponent::startAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::pauseAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::resetAnimation(std::string animation)
+{
+}
+
+void AnimationComponent::update(const float& dt)
 {
 }
