f6720833fcc9e098fa9b3cfac9ec724a761f0c14
Daniel Horn <danielhorn101@gmail.com>
2020-07-09 16:46:02 +0200

    Finalizing animations

--- AnimationComponent.cpp
+++ AnimationComponent.cpp
@@ -1,7 +1,7 @@
 #include "AnimationComponent.h"
 
 AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet)
-	: sprite(sprite), textureSheet(texture_sheet)
+	: sprite(sprite), textureSheet(texture_sheet), lastAnimation(nullptr)
 {
 	std::cout << "AnimationComponent: " << &this->textureSheet << "\n";
 	std::cout << "AnimationComponent: " << &texture_sheet << "\n";
@@ -28,5 +28,16 @@ void AnimationComponent::addAnimation(const std::string key,
 
 void AnimationComponent::play(const std::string key, const float& dt)
 {
+	if (this->lastAnimation != this->animations[key])
+	{
+		if (this->lastAnimation == nullptr) {
+			this->lastAnimation = this->animations[key];
+		}
+		else {
+			this->lastAnimation->reset();
+			this->lastAnimation = this->animations[key];
+		}
+		
+	}
 	this->animations[key]->play(dt);
 }
