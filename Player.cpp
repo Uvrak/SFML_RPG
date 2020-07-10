641d50c5c0b0f071c4e6b9c5ed06d094ace12c82
Daniel Horn <danielhorn101@gmail.com>
2020-07-10 09:50:59 +0200

    priority animation part3

--- Player.cpp
+++ Player.cpp
@@ -31,50 +31,86 @@ Player::~Player()
 }
 
 //Functions
-void Player::update(const float& dt)
+void Player::updateAttack()
 {
-	this->movementComponent->update(dt);
-
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 	{
 		this->attacking = true;
-		
-	}
 
+	}
+}
+
+void Player::updateAnimation(const float& dt)
+{
 	if (this->attacking)
 	{
-		if(this->animationComponent->play("ATTACK", dt, true))
+		//set origin depending on facing direction
+		if (this->sprite.getScale().x > 0.f) //facing left
+		{
+			this->sprite.setOrigin(96.f, 0.f);
+		}
+		else //Facing right
+		{
+			this->sprite.setOrigin(258.f + 96.f, 0.f);
+		}
+		//Animate and check for animation end
+		if (this->animationComponent->play("ATTACK", dt, true))
+		{
 			this->attacking = false;
-			
+			if (this->sprite.getScale().x > 0.f) //facing left
+			{
+				this->sprite.setOrigin(0.f, 0.f);
+			}
+			else //Facing right
+			{
+				this->sprite.setOrigin(258.f, 0.f);
+			}
+		}
 	}
-	
+
 	if (this->movementComponent->getState(IDLE))
 		this->animationComponent->play("IDLE", dt);
-	
+
 	else if (this->movementComponent->getState(MOVING_LEFT))
 	{
-		this->sprite.setOrigin(0.f, 0.f);
-		this->sprite.setScale(1.f, 1.f);
+		if (this->sprite.getScale().x < 0.f)
+		{
+			this->sprite.setOrigin(0.f, 0.f);
+			this->sprite.setScale(1.f, 1.f);
+		}
+
 		this->animationComponent->play("WALK", dt);
 	}
-		
-	else if (this->movementComponent->getState(MOVING_RIGHT)) 
+
+	else if (this->movementComponent->getState(MOVING_RIGHT))
 	{
-		this->sprite.setOrigin(258.f, 0.f);
-		this->sprite.setScale(-1.f, 1.f);
+		if (this->sprite.getScale().x > 0.f) {
+			this->sprite.setOrigin(258.f, 0.f);
+			this->sprite.setScale(-1.f, 1.f);
+		}
+
 		this->animationComponent->play("WALK", dt, this->movementComponent->getVelocity().x, this->movementComponent->getMaxVelocity());
 	}
-	
-	else if (this->movementComponent->getState(MOVING_UP)) 
+
+	else if (this->movementComponent->getState(MOVING_UP))
 	{
 		this->animationComponent->play("WALK", dt);
 	}
-	
+
 	else if (this->movementComponent->getState(MOVING_DOWN))
 	{
 		this->animationComponent->play("WALK", dt);
 	}
+}
 
+//Functions
+void Player::update(const float& dt)
+{
+	this->movementComponent->update(dt);
+
+	this->updateAttack();
+
+	this->updateAnimation(dt);
 
 	this->hitboxComponent->update();
 }
